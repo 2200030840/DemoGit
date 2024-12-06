85cd8e3547beadb058889d4234b8f4bc68997689
2200030840 <ksrprabhas@gmail.com>
2024-12-06 23:10:36 +0530

    V version2

--- src/main/java/com/klef/springboot/SpringBootFirstProjectApplication.java
+++ src/main/java/com/klef/springboot/SpringBootFirstProjectApplication.java
@@ -8,7 +8,7 @@ public class SpringBootFirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirstProjectApplication.class, args);
-		System.out.println("Hi Spring Boot, How are You ? We are going to play with you for next one month");
+		System.out.println("Hi Spring Boot");
 	}
 
 }
